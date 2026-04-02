7f162f18aa532ee8a678e077b5700b3d3cdbe46b
Chavda Kishan <chavdakishan292@gmail.com>
2026-04-02 13:55:46 +0530

    Update auth_utils.py

--- backend/utils/auth_utils.py
+++ backend/utils/auth_utils.py
@@ -17,8 +17,10 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1440)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBearer()
 
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+# def hash_password(password: str) -> str:
+    # return pwd_context.hash(password)
+def hash_password(password: str):
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
